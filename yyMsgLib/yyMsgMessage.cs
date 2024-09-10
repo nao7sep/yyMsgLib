c3d02bf5fc1998cd2abd45bb8fc875a72f2372d0
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-09-10 11:13:51 +0900

    Add yyMsgTopic and yyMsgMessage

---
+++ yyMsgLib/yyMsgMessage.cs
@@ -0,0 +1,28 @@
+﻿namespace yyMsgLib
+{
+    public class yyMsgMessage
+    {
+        public int? Id { get; init; }
+        public Guid Guid { get; init; }
+
+        public yyMsgTopic? Topic { get; init; }
+        public int? TopicId { get; init; }
+        public Guid TopicGuid { get; init; }
+
+        public DateTime CreatedAtUtc { get; init; }
+        public yyMsgUser? CreatedByUser { get; init; }
+        public int? CreatedByUserId { get; init; }
+        public Guid CreatedByUserGuid { get; init; }
+
+        public required string Content { get; set; } // Updatable
+
+        public bool IsPinned { get; set; }
+        public DateTime? PinnedAtUtc { get; set; }
+        public yyMsgUser? PinnedByUser { get; set; }
+        public int? PinnedByUserId { get; set; }
+        public Guid PinnedByUserGuid { get; set; }
+
+        public List <yyMsgAttachment> Attachments { get; init; } = [];
+        public int AttachmentCount { get; set; }
+    }
+}
---
+++ yyMsgLib/yyMsgTopic.cs
@@ -0,0 +1,19 @@
+﻿namespace yyMsgLib
+{
+    public class yyMsgTopic
+    {
+        public int? Id { get; init; }
+        public Guid Guid { get; init; }
+
+        public DateTime CreatedAtUtc { get; init; }
+
+        public required string Name { get; set; } // Updatable
+
+        public bool IsArchived { get; set; }
+        public DateTime? ArchivedAtUtc { get; set; }
+
+        public List <yyMsgMessage> Messages { get; init; } = [];
+        public int MessageCount { get; set; }
+        public DateTime? LastMessageCreatedAtUtc { get; set; }
+    }
+}
